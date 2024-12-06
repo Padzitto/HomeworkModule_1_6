72151cb79f6565bcbc5b46a3f52872b61e8c437b
Padzitto <mokrienkousa@gmail.com>
2024-12-06 15:06:52 +1000

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,7 @@
+# переменные camecase DateOfBirth snakecase date_of_birth
+name = "Python" #Название курса
+TotalTasks = 12 # Количество выполненных ДЗ
+spent_hours = 1.5 # Количество затраченных часов
+_average_time = spent_hours/TotalTasks # Время на одно задание (вычислить используя 1 и 2 переменные)
+print("Курс:",name,",", "всего задач:", TotalTasks, ",", "затрачено часов:", spent_hours, ",","среднее время выполнения", _average_time, "часа.")
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
